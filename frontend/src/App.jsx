224cac3bfe756af060c10153a8ea25fe17e4b9cb
Guille <21146865+ProfGuille@users.noreply.github.com>
2025-04-15 19:13:40 -0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeList from "./components/EmployeeList";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Gestión de Empleados</h1>
+      <EmployeeList />
+    </div>
+  );
+};
+
+export default App;
